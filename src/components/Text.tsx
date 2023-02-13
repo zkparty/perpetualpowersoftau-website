1e7cf844532da6e035e177b581e235802b0c4a16
Nico Serrano <nnicosp@hotmail.com>
2023-02-13 17:21:34 -0500

    design(fix): better animations

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -5,11 +5,19 @@ import { textDescriptions, textTitles } from '../style/utils'
 
 
 const H1 = styled.h1`
+    letter-spacing: 3px;
     text-transform: uppercase;
     font-size: ${FONT_SIZE.XXL};
     font-weight: 600;
     ${textTitles}
     margin: 0px;
+
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 1000ms;
+    &:hover {
+        filter: drop-shadow(0 0 10px #646cffaa);
+    }
 `
 
 const Description = styled.p`
