7e2a29e553dc3d0b4f21afda999c1ec16c767a81
Nico Serrano <nnicosp@hotmail.com>
2023-04-03 19:12:03 -0500

    error(feat): add timeout

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
 import { FONT_SIZE } from '../constants';
 import { textDescriptions } from '../style/utils';
@@ -8,11 +8,25 @@ type Props = {
 }
 
 const ErrorMessage = ({ children }: Props) => {
+    const [show, setShow] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShow(false)
+        }, 7000) // 7 seconds
+        return () => clearTimeout(timer)
+    }, [])
+
     return (
-        <Container>
-            {children}
-        </Container>
-    );
+        <>
+        {show ?
+            <Container>
+                {children}
+            </Container>
+        :
+        null}
+        </>
+    )
 }
 
 const Container = styled.div`
